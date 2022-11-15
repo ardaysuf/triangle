71cfe960730189c59bba1ca98792f09c8ea1081a
ardaysuf <117676232+ardaysuf@users.noreply.github.com>
2022-11-15 15:05:32 +0300

    Add files via upload

---
+++ aandpoftriangle.py
@@ -0,0 +1,31 @@
+
+a = int(input("Enter the First Length of Side: "))
+b = int(input("Enter the Second Length of Side: "))
+r = int(input("Enter the Third Length of Run: "))
+
+if (a + b < r or abs(a - b) > r):
+    print("Invalid Values...")
+    quit()
+elif (b + r < a or abs(b - r) > a):
+    print("Invalid Values...")
+    quit()
+elif (a + r < b or abs(a - r) > b):
+    print("Invalid Values...")
+    quit()        
+else:
+    if (a == b  and b != r):
+        print("Isosceles Triangle")
+    elif (a == b and b == r):
+        print("Equilateral Triangle")
+    else:
+        print("Scalene Triangle")       
+
+perimeter = a + b + r
+
+h = int(input("Enter the Lenght of Height: "))
+
+area = (r * h) / 2
+
+print("Perimeter of the Triangle = " + str(perimeter))
+print("Area of the Triangle = " + str(area))
+
